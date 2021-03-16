e220d024e053c6485fed9c67c337cdd5da767ffe
Fernando <al365081@uji.es>
2021-03-16 23:30:51 +0100

    Primer minijuego temperatura
    Primeros pasos y arreglo errores

---
+++ Assets/Scripts/Temperature/EnemyMovementTemp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementTemp : MonoBehaviour
+{
+    public float speed;
+    public float destroyingDistance;
+
+    private Transform target;
+
+    void Start(){
+        target = GameObject.FindGameObjectWithTag("Objective").GetComponent<Transform>();
+    }
+
+    void Update(){
+        if(Vector2.Distance(transform.position, target.position) > destroyingDistance){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        switch (col.gameObject.tag){
+            case "Player":
+                Destroy (gameObject);
+                break;
+            
+            case "Objective":
+                Destroy (gameObject);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Temperature/EnemySpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX;
+    float randY;
+    Vector2 whereToSpawn;
+    //Time they take to spawn
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Update(){
+        if(Time.time > nextSpawn){
+            nextSpawn = Time.time +spawnRate;
+            randX = Random.Range (-8.4f, 8.4f);
+            randY = Random.Range (-3f, 3f);
+            whereToSpawn = new Vector2(randX, randY);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+    /*
+    public Transform[] spawnPoints;
+	public GameObject[] monsters;
+	int randomSpawnPoint, randomMonster;
+	public static bool spawnAllowed;
+
+	// Use this for initialization
+	void Start () {
+		spawnAllowed = true;
+		InvokeRepeating ("SpawnAMonster", 0f, 1f);
+	}
+
+	void SpawnAMonster()
+	{
+		if (spawnAllowed) {
+			randomSpawnPoint = Random.Range (0, spawnPoints.Length);
+			randomMonster = Random.Range (0, monsters.Length);
+			Instantiate (monsters [randomMonster], spawnPoints [randomSpawnPoint].position,
+				Quaternion.identity);
+		}
+	}
+    */
+}
