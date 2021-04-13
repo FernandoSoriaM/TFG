358c2c6072e2813d163acc078787ca228016c96a
Fernando <al365081@uji.es>
2021-04-13 19:03:26 +0200

    Añadir otro minigame
    ++

---
+++ Assets/Scripts/X-Ray Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool isDrag;
+
+    public void OnMouseDown() {
+        isDrag = true;        
+    }
+    public void OnMouseUp() {
+        isDrag = false;          
+    }
+    void Update(){
+        if (isDrag){
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
